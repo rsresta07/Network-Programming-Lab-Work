7452b73e1ce04ffaa5203bab9db90a3bdfa0a77d
Rameshwor Shrestha <r.sresta07@gmail.com>
2024-08-28 13:16:30 +0545

    Complete RMI

---
+++ Multicast/Client.java
@@ -0,0 +1,33 @@
+package Multicast;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+public class Client {
+    public static void main(String[] args) {
+        String multicastAddress = "230.0.0.0";
+        int port = 4446;
+
+        try {
+            InetAddress group = InetAddress.getByName(multicastAddress);
+            MulticastSocket socket = new MulticastSocket(port);
+            socket.joinGroup(group);
+
+            System.out.println("Joined multicast group, waiting for messages...");
+
+            byte[] buffer = new byte[256];
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+            while (true) {
+                socket.receive(packet);
+                String received = new String(packet.getData(), 0, packet.getLength());
+                System.out.println("Message: " + received);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Multicast/Server.java
@@ -0,0 +1,30 @@
+package Multicast;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+public class Server {
+    public static void main(String[] args) {
+        String multicastMessage = "Hello";
+        String multicastAddress = "230.0.0.0";
+        int port = 4446;
+
+        try {
+            InetAddress group = InetAddress.getByName(multicastAddress);
+            MulticastSocket socket = new MulticastSocket();
+
+            byte[] buffer = multicastMessage.getBytes();
+
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, group, port);
+            socket.send(packet);
+
+            System.out.println("Message sent to multicast group: " + multicastMessage);
+
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
